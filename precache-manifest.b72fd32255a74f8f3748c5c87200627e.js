517fd77a9c959c2d1248989c4e4843377ded4be2
SolidRocker <kamtk.tyler@gmail.com>
2019-03-16 20:25:45 +0800

    Updates

---
+++ precache-manifest.b72fd32255a74f8f3748c5c87200627e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "29d6ed25805f6d135ac76facfee94662",
+    "url": "/find-food/static/media/thesmartlocal.29d6ed25.png"
+  },
+  {
+    "revision": "56e829f8d70d453e1dff842c7fe2cf41",
+    "url": "/find-food/static/media/star.56e829f8.png"
+  },
+  {
+    "revision": "5517c24f45051d56b15ad39f0392a93c",
+    "url": "/find-food/static/media/instagram.5517c24f.png"
+  },
+  {
+    "revision": "4a668b2f45ea1289291c70df3187e1f1",
+    "url": "/find-food/static/media/facebook.4a668b2f.png"
+  },
+  {
+    "revision": "43510351d3ca5416375381ea3fee8422",
+    "url": "/find-food/static/media/eatbook.43510351.png"
+  },
+  {
+    "revision": "d61920a27aa7e38cbe85",
+    "url": "/find-food/static/js/runtime~main.81ac81fa.js"
+  },
+  {
+    "revision": "be2e31d182ce82ed3023",
+    "url": "/find-food/static/js/main.d9de3732.chunk.js"
+  },
+  {
+    "revision": "77a3b0d4edfe83ca9276",
+    "url": "/find-food/static/js/2.6a4ac157.chunk.js"
+  },
+  {
+    "revision": "be2e31d182ce82ed3023",
+    "url": "/find-food/static/css/main.3f365d12.chunk.css"
+  },
+  {
+    "revision": "d0390302ff732b8d7e041c2c8e9457a8",
+    "url": "/find-food/index.html"
+  }
+];
